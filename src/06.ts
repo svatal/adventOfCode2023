4108339f1a200a1df645d604a3bf4b83348919eb
svatal <swata@centrum.cz>
2023-12-07 00:11:51 +0100

    06 - do not allocate huge array

--- src/06.ts
+++ src/06.ts
@@ -1,6 +1,5 @@
 // import { testInput as input } from "./06-input";
 import { input } from "./06-input";
-import { prefillArray } from "./utils/util";
 
 export function doIt(progress: (...params: any[]) => void) {
   const [times, distances] = input.split(`\n`).map((line) => {
@@ -8,21 +7,24 @@ export function doIt(progress: (...params: any[]) => void) {
     return values.map(Number);
   });
   const first = times
-    .map((raceTime, i) => {
-      const maxDistance = distances[i];
-      return prefillArray(raceTime, (time) => time * (raceTime - time)).filter(
-        (d) => d > maxDistance
-      ).length;
-    })
+    .map((raceTime, i) => getWins(raceTime, distances[i]))
     .reduce((a, b) => a * b, 1);
 
   const [raceTime, maxDistance] = input.split(`\n`).map((line) => {
     const [, value] = line.replace(/\s+/g, "").split(":");
     return +value;
   });
-  const second = prefillArray(
-    raceTime,
-    (time) => time * (raceTime - time)
-  ).filter((d) => d > maxDistance).length;
+  const second = getWins(raceTime, maxDistance);
   console.log(first, second);
+}
+
+function getWins(raceTime: number, maxDistance: number) {
+  let wins = 0;
+  for (let time = 0; time <= raceTime; time++) {
+    const distance = time * (raceTime - time);
+    if (distance > maxDistance) {
+      wins++;
+    }
+  }
+  return wins;
 }
